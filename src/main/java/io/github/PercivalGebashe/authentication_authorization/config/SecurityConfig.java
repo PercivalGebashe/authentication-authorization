7407300fa04efa9aadc4c8b99f78e97ed2735566
Percival_Gebashe <percivalgebashe@gmail.com>
2025-09-10 17:31:24 +0200

    Minor fix permitted all on confirm-delete endpoint

--- src/main/java/io/github/PercivalGebashe/authentication_authorization/config/SecurityConfig.java
+++ src/main/java/io/github/PercivalGebashe/authentication_authorization/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/api/v1/user/confirm-delete").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
